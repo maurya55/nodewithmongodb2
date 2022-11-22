61294de7f9c7954d5f48aa497acfb8e207e1dfce
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-22 19:11:34 +0530

    add

--- controller.js
+++ controller.js
@@ -46,7 +46,7 @@ module.exports = {
             await client.disconnect();
             return res.status(200).json({
                 redisstatus,
-                message: "get data",
+                message: "get data ng2",
                 data: getData
             })
         }
