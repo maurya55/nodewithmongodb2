6f2b72d62607289ab34e98a81049e8e5490029a5
Rahulmaurya55 <rahulmaurya55.rm@gmail.com>
2022-11-24 10:08:29 +0530

    rabbit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require('dotenv').config()
-
+const amqplib = require("amqplib");
 
 
 const port = 4300;
@@ -28,6 +28,53 @@ app.use((err, req, res, next) => {
     })
 })
 
+
+async function rabbit() {
+    const queue = 'tasks';
+    const conn = await amqplib.connect('amqp://localhost');
+    // const conn = await amqplib.connect('amqps://xkqumufe:flJcTjPT68JSo4pVt2Fk249iWCtNi4GH@rabbit.lmq.cloudamqp.com/xkqumufe');
+
+    // const conn = await amqplib.connect({
+    //     protocol: 'amqp',
+    //     port: 5672,
+    //     host: "localhost:2000/",
+    //     // username: config.user,
+    //     // password: config.password,
+    //     frameMax: 4096
+    // });
+
+    const ch1 = await conn.createChannel();
+    await ch1.assertQueue(queue);  
+  
+    // Listener
+    ch1.consume(queue, (msg) => {
+        // console.log(msg)
+        if (msg !== null) {
+            let message = JSON.parse(msg.content.toString())
+            // console.log(message);
+            if (message.sendTo == "nodeWithMongodb2") { 
+                console.log('Recieved:', message);
+                ch1.ack(msg);
+            }
+        } else {
+            console.log('Consumer cancelled by server');
+        }
+    });
+ 
+    // Sender
+    const ch2 = await conn.createChannel();
+
+    
+
+    setInterval(() => {
+        let message = {
+            sendTo: "nodeWithMongodb",
+            data: `data send from nodeWithMongodb2 ${Date.now()}`,
+        }
+        ch2.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+    }, 1200);
+}
+rabbit();
 
 // mongoose.connect('mongodb://localhost:27018/nodewithmongodb', {
 //     useNewUrlParser: true,
